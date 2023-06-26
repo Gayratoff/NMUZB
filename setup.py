5b4c3f7b4beb5dbf3bc3658324150c186bf7fd33
Xudoberdi G'ayratov <115026746+Gayratoff@users.noreply.github.com>
2023-06-26 12:50:46 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nmuzb',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[''],
+    author='XUDOBERDI GAYRATOV',
+    author_email='xudoberdigayratov@gmail.com',
+    description="O'zbek ismlarining to'liq, batafsil ma'nosi, kelib chiqishi, shakllari. O'zingiz va yaqinlaringizni ismlari ma'nosini bilib oling.",
+    url='https://github.com/Gayratoff/NMUZB.git',
+)
